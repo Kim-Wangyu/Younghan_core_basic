c5bb437e3f01cce86351e6980bda81992d2db5a7
Kim-Wangyu <wang90205@gmail.com>
2022-09-22 10:01:34 +0900

    새로운 할인정책 RateDiscount

--- src/main/java/com/basic/core/discount/DiscountPolicy.java
+++ src/main/java/com/basic/core/discount/DiscountPolicy.java
@@ -4,7 +4,7 @@ import com.basic.core.member.Member;
 
 public interface DiscountPolicy {
     /**
-     * @return 할인 대상 금액 (천원이 할인되면 천원을 리턴
+     * @return 할인 대상 금액 (천원이 할인되면 천원을 리턴)
      **/
     int discount(Member member, int price);
 
